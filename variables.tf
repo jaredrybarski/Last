ca0897d1a91703f18d4d14d88cf7be8bb502bd24
Jared Rybarski <jarko92@gmail.com>
2022-10-10 20:58:34 +0200

    4th commit, profile

--- variables.tf
+++ variables.tf
@@ -35,14 +35,3 @@ variable "vpc_enable_nat_gateway" {
 }
 
 
-variable "access_key" {
-  type        = string
-  description = "access key"
-  default     = "AKIAZJ4Y6W6NU3PEBJOG"
-}
-
-variable "secret_key" {
-  type        = string
-  description = "secret key"
-  default     = "DGLQkFD+wUIL38h44MfkDb6e0dzUA2p/coF4LahM"
-}
